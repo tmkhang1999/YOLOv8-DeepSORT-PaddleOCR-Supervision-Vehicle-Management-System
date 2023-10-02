0c0507e3c4d0a9757f5e3dd87d62479c2ae3e457
MINH KHANG TRAN <74235084+tmkhang1999@users.noreply.github.com>
2023-10-02 15:27:15 +0700

    Reconstruct files

---
+++ modules/__init__.py
@@ -0,0 +1,3 @@
+from .plate_recognition import PlateRecognizer
+from .speed_estimation import SpeedEstimator
+from .video_processor import VideoProcessor
--- modules/video_processor.py
+++ modules/video_processor.py
@@ -1,3 +1,6 @@
+import logging
+import math
+
 from tqdm.notebook import tqdm
 import numpy as np
 from collections import deque
@@ -13,37 +16,38 @@ from supervision.video.source import get_video_frames_generator
 from supervision.geometry.dataclasses import Point
 from supervision.draw.color import ColorPalette
 
-from modules.annotation import draw_note_box, draw_plate_box, draw_tracking_line
-from modules.plate_recognition import recognize_plate
-from modules.speed_estimation import compute_vel
+from modules import PlateRecognizer, SpeedEstimator
+from utils.annotation import NoteAnnotator, TraceAnnotator
+
+log = logging.getLogger(__name__)
 
 
 class VideoProcessor:
     def __init__(self, source_video_path, target_video_path, cfg):
+        """
+        Initialize the VideoProcessor.
+
+        Args:
+            source_video_path (str): Path to the source video.
+            target_video_path (str): Path to the target video.
+            cfg (dict): Configuration dictionary.
+        """
         self.source_video_path = source_video_path
         self.target_video_path = target_video_path
+        self.config = cfg
+        self.V0 = cfg['camera']['V0']
+        self.y_min = cfg['camera']['y_min']
         self.data_tracker = {}
         self.CLASS_ID = [2, 3, 5, 7]
 
-        # Get the camera's information
-        self.s_x = cfg['s_x']
-        self.s_y = cfg['s_y']
-        self.f1 = cfg['f1']
-        self.f2 = cfg['f2']
-        self.l0 = cfg['L0']
-
-        # Specify H matrix to rectify videos of each location
-        self.H = [[self.l0, -self.l0 * (self.f1 / self.f2), 0.0], [0.0, 1.0, 0.0], [0.0, -(1 / self.f2), 1.0]]
-        self.y_min = cfg['y_min']
-        self.y_max = cfg['y_max']
-        self.V0 = cfg['V0']
-
     def setup(self):
-        # Initialize the vehicle detection
-        self.vehicle_detector = YOLO("../data/models/yolov8x.pt")
-        self.CLASS_DICT = {}
-        for id in self.CLASS_ID:
-            self.CLASS_DICT[id] = self.vehicle_detector.model.names[id]
+        """
+           Set up the video processing environment and initialize necessary components.
+        """
+        # Initialize the vehicle detection/ plate detection/ paddleOCR models
+        self.vehicle_detector = YOLO(self.config['models']['vehicle_detector_path'])
+        self.plate_detector = YOLO(self.config['models']['plate_detector_path'])
+        self.ocr_model = PaddleOCR(lang='en', show_log=False, use_angle_cls=True, use_gpu=False)
 
         # Initialize the tracker
         self.object_tracker = DeepSort(max_age=20,
@@ -61,34 +65,50 @@ class VideoProcessor:
                                        polygon=False,
                                        today=None)
 
-        # Initialize the license_plate_detector
-        self.plate_detector = YOLO("../data/models/license_plate_detector.pt")
+        # Create an ID dictionary for each vehicle type
+        self.CLASS_DICT = {}
+        for id in self.CLASS_ID:
+            self.CLASS_DICT[id] = self.vehicle_detector.model.names[id]
 
-        # Initialize the Paddle OCR
-        self.ocr_model = PaddleOCR(lang='en', show_log=False, use_angle_cls=True, use_gpu=False)
-
-        # create VideoInfo instance
+        # Create VideoInfo instance, frame generator
         self.video_info = VideoInfo.from_video_path(self.source_video_path)
-        # create frame generator
         self.generator = get_video_frames_generator(self.source_video_path)
 
-        # create LineCounter instance
-        LINE_START = Point(50, 1300)
-        LINE_END = Point(3840 - 50, 1300)
+        # Create LineCounter instance
+        width, height = self.video_info.resolution
+        LINE_START = Point(50, int(height * 0.6))
+        LINE_END = Point(width - 50, int(height * 0.6))
         self.line_counter = LineCounter(start=LINE_START, end=LINE_END)
-        # create instance of BoxAnnotator and LineCounterAnnotator
+
+        # Create instance of BoxAnnotator and LineCounterAnnotator
         self.box_annotator = BoxAnnotator(color=ColorPalette(), thickness=4, text_thickness=4, text_scale=1)
         self.line_annotator = LineCounterAnnotator(thickness=4, text_thickness=4, text_scale=2)
 
+        # Create instance of PlateRecognizer and SpeedEstimator
+        self.plate_recognizer = PlateRecognizer(license_plate_detector=self.plate_detector, ocr_model=self.ocr_model)
+        self.speed_estimator = SpeedEstimator(cfg=self.config['camera'], fps=self.video_info.fps)
+
+        # Create instance of NoteAnnotator and NoteAnnotator
+        self.note_annotator = NoteAnnotator(color_dict=self.CLASS_DICT)
+        self.trace_annotator = TraceAnnotator()
+
+        # Calculate the scale of our ơn frame compared to 720p (1280 x 720px)
+        self.scale = int(math.sqrt(width * height / 1280 / 720))
+
     def process_video(self):
+        """
+           Process the video, including detection, tracking, speed estimation, vehicle counting and annotation.
+        """
         with VideoSink(self.target_video_path, self.video_info) as sink:
             frame_count = 0
             for frame in tqdm(self.generator, total=self.video_info.total_frames):
-                draw_note_box(frame, self.CLASS_DICT)
-                # detect vehicle on a single frame
+                # Draw notes in the upper left of the frame
+                self.note_annotator.annotate(frame)
+
+                # Detect vehicle on a single frame
                 results = self.vehicle_detector(frame, conf=0.6)
 
-                # tracking detections
+                # Tracking detections
                 dets = []
                 for x1, y1, x2, y2, conf, id in results[0].boxes.data.cpu().numpy():
                     if id in self.CLASS_ID:
@@ -109,27 +129,28 @@ class VideoProcessor:
                             tracker_id=np.array(track_ids).astype(int)
                         )
 
-                        # filter not confirmed
+                        # Filter not confirmed
                         detections.filter(mask=mask, inplace=True)
 
                         labels = []
                         for xyxy, confidence, class_id, tracker_id in detections:
                             if tracker_id not in self.data_tracker:
-                                # Initialize the tracker state if it doesn't exist
-                                self.data_tracker[tracker_id] = [self.V0, 0, frame_count, deque(maxlen=64), None,
-                                                                 0]  # [speed, step, frame_count, center_point, plate, ocr_conf]
+                                # Initialize the tracker state
+                                # if it doesn't exist [speed, step, frame_count, center_points_buffer, plate, ocr_conf]
+                                self.data_tracker[tracker_id] = [self.V0, 0, frame_count, deque(maxlen=64), None, 0]
 
-                            # add center to buffer
+                            # Add center_point to buffer
                             x1, y1, x2, y2 = [int(i) for i in xyxy]
-                            center = (int((x2 + x1) / 2), int((y1 + y2) / 2))
-                            self.data_tracker[tracker_id][3].appendleft(center)
+                            center_point = (int((x2 + x1) / 2), int((y1 + y2) / 2))
+                            self.data_tracker[tracker_id][3].appendleft(center_point)
 
                             speed_label = ""
-                            # Check y threshold
+                            tracker_state = self.data_tracker[tracker_id]
+                            # Check y threshold for speed estimation and plate detection
                             if xyxy[1] > self.y_min:
-                                tracker_state = self.data_tracker[tracker_id]
-                                prev_speed, step, frame_num = tracker_state[0], tracker_state[1], frame_count - \
-                                                              tracker_state[2]
+                                prev_speed = tracker_state[0]
+                                step = tracker_state[1]
+                                frame_num = frame_count - tracker_state[2]
 
                                 # Estimate the speed of vehicle
                                 if step == 0:
@@ -141,11 +162,10 @@ class VideoProcessor:
                                     box_vel = np.array(data_deque[0]) - np.array(data_deque[1])
 
                                     # Calculate the new speed
-                                    speed = compute_vel(xyxy, box_vel, step, frame_num, prev_speed,
-                                                        self.s_x, self.s_y, self.y_min, self.y_max,self.H,
-                                                        self.video_info.fps, scale=3)
+                                    speed = self.speed_estimator.compute_vel(xyxy, box_vel, step, frame_num, prev_speed,
+                                                                             self.scale)
 
-                                    # Update the tracker
+                                    # Update the tracker_state
                                     tracker_state[0] = speed
                                     tracker_state[1] += 1
                                     tracker_state[2] = frame_count
@@ -154,30 +174,31 @@ class VideoProcessor:
                                     speed_label = f" | Speed: {tracker_state[0]:0.2f} mph"
 
                                 # Recognize plate
-                                plate, ocr_conf = recognize_plate(frame, xyxy)
+                                plate, ocr_conf = self.plate_recognizer.detect(frame, xyxy)
                                 if plate is not None and ocr_conf > tracker_state[-1]:
                                     tracker_state[-2], tracker_state[-1] = plate, ocr_conf
 
-                                # Visualize the plate
+                                # Visualize plate
                                 if tracker_state[-2] is not None:
                                     text = f"[{tracker_state[-2]}] Conf: {tracker_state[-1]:0.2f}"
-                                    frame = draw_plate_box(frame, xyxy, text)
+                                    frame = self.plate_recognizer.annotate(frame, xyxy, text)
 
                             # Update labels
                             labels.append(
-                                f"[{tracker_id}] {self.CLASS_DICT[class_id].capitalize()} | Conf: {confidence:0.2f}" + speed_label)
+                                f"[{tracker_id}] {self.CLASS_DICT[class_id].capitalize()} "
+                                f"| Conf: {confidence:0.2f}" + speed_label)
 
-                            # draw the tracking line
-                            draw_tracking_line(frame, self.data_tracker[tracker_id][3], class_id)
+                            # Annotate tracking trail
+                            self.trace_annotator.annotate(frame, tracker_state[3], class_id)
 
-                        # updating line counter
+                        # Update line counter
                         self.line_counter.update(detections=detections)
 
-                # annotate and display frame
+                # Annotate and display frame
                 frame = self.box_annotator.annotate(frame=frame, detections=detections, labels=labels)
-                frame_count += 1
-
                 self.line_annotator.annotate(frame=frame, line_counter=self.line_counter)
+
+                frame_count += 1
                 sink.write_frame(frame)
 
-        print("DONE")
+        log.info("___________________________DONE___________________________")
