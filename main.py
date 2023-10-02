cfb5ab2311d34f576feae4f8d4a09f2bcd88f543
MINH KHANG TRAN <74235084+tmkhang1999@users.noreply.github.com>
2023-10-02 15:27:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
-from utils.video_processor import VideoProcessor
+import argparse
+from modules import VideoProcessor
+from utils.config import ConfigManager
+
+
+def main(args):
+    """
+    Process a video using the specified configuration.
+
+    Args:
+        args (argparse.Namespace): Command-line arguments.
+    """
+    # Load the configuration
+    config_manager = ConfigManager(args.config_path)
+    config = config_manager.load_config()
+
+    # Initialize and setup the video processor
+    processor = VideoProcessor(source_video_path=args.source_video,
+                               target_video_path=args.target_video,
+                               cfg=config)
+    processor.setup()
+
+    # Process the video
+    processor.process_video()
+
 
 if __name__ == "__main__":
-    cfg = {'V0': 75.0, 's_x': 3.2 / 5, 's_y': 3, 'y_min': 750.0, 'y_max': 2500.0,
-           'L0': 0.2, 'f1': 928.7943, 'f2': -67.777}
-    processor = VideoProcessor("./data/videos/vehicle-counting.mp4", "./data/videos/output.mp4", cfg)
-    processor.setup()
-    processor.process_video()
+    # Create argument parser
+    parser = argparse.ArgumentParser(description="Process a video with vehicle tracking and counting.")
+
+    # Define command-line arguments with default values
+    parser.add_argument("--config-path", default="./utils/config.yml",
+                        help="Path to the configuration file (config.yml).")
+    parser.add_argument("--source-video", default="./data/videos/vehicle-counting.mp4",
+                        help="Path to the source video file.")
+    parser.add_argument("--target-video", default="./data/videos/output.mp4",
+                        help="Path to the target video file for the output.")
+
+    # Parse command-line arguments
+    args = parser.parse_args()
+
+    # Call the main function with parsed arguments
+    main(args)
