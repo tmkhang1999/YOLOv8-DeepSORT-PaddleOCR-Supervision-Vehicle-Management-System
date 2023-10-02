750d73d19b0f1ea7f3b71b4ce5ee77775120c7bd
MINH KHANG TRAN <74235084+tmkhang1999@users.noreply.github.com>
2023-10-02 15:25:57 +0700

    Create SpeedEstimator class

--- modules/speed_estimation.py
+++ modules/speed_estimation.py
@@ -1,39 +1,93 @@
 import numpy as np
 
-def compute_vel(det, box_vel, step, frame_num, prev_speed, s_x, s_y, y_min, y_max, H, fps, scale):
-    bottom_center = np.array([(det[0] + det[2]) / 2.0, (det[3] - y_min)]) / scale
-    box_vel = box_vel / scale
 
-    # Transform the Velocities using the computed camera to real world equations in the paper
-    v_x_trans = (((H[0][0] * box_vel[0] + H[0][1] * box_vel[1]) * (
-                H[2][0] * bottom_center[0] + H[2][1] * bottom_center[1] + H[2][2]) -
-                  (H[0][0] * bottom_center[0] + H[0][1] * bottom_center[1] + H[0][2]) * (
-                              H[2][0] * box_vel[0] + H[2][1] * box_vel[1])) /
-                 ((H[2][0] * bottom_center[0] + H[2][1] * bottom_center[1] + H[2][2]) ** 2))
+class SpeedEstimator:
+    """
+    A class for estimating the speed of an object based on 2D images.
 
-    v_y_trans = (((H[1][0] * box_vel[0] + H[1][1] * box_vel[1]) * (
-                H[2][0] * bottom_center[0] + H[2][1] * bottom_center[1] + H[2][2]) -
-                  (H[1][0] * bottom_center[0] + H[1][1] * bottom_center[1] + H[1][2]) * (
-                              H[2][0] * box_vel[0] + H[2][1] * box_vel[1])) /
-                 ((H[2][0] * bottom_center[0] + H[2][1] * bottom_center[1] + H[2][2]) ** 2))
+    Args:
+        cfg (dict): Configuration parameters containing camera and transformation details.
+        fps (int): Frames per second of the video stream.
+    """
 
-    # Scale Recovery
-    s1, s2 = 1.1, 0.9
-    a, b = (s2 - s1) / (y_max - y_min) * scale, s1
-    s = a * bottom_center[1] + b
+    def __init__(
+            self,
+            cfg: dict,
+            fps: int):
+        # Get the camera's information
+        self.s_x = cfg['s_x']
+        self.s_y = cfg['s_y']
+        self.f1 = cfg['f1']
+        self.f2 = cfg['f2']
+        self.l0 = cfg['L0']
 
-    # Velocity calculation
-    instant_speed = np.sqrt(sum([(v_x_trans * s_x) ** 2, (v_y_trans * s_y) ** 2]))
+        # Specify H matrix to rectify videos of each location
+        self.H = [
+            [self.l0, -self.l0 * (self.f1 / self.f2), 0.0],
+            [0.0, 1.0, 0.0],
+            [0.0, -(1 / self.f2), 1.0]
+        ]
+        self.y_min = cfg['y_min']
+        self.y_max = cfg['y_max']
+        self.V0 = cfg['V0']
+        self.fps = fps
 
-    # translating the speed to miles/hour
-    vi = instant_speed / frame_num * fps * 9 / 4
+    def compute_vel(
+            self,
+            det: np.ndarray,
+            box_vel: np.ndarray,
+            step: int,
+            frame_num: int,
+            prev_speed: float,
+            scale: float
+    ) -> float:
+        """
+        Compute the estimated velocity of an object based on detected velocities.
 
-    # Suppress noise
-    if vi <= 3.0:
-        vi = 0.0
+        Args:
+            det (np.ndarray): Bounding box coordinates [x1, y1, x2, y2].
+            box_vel (np.ndarray): Velocity vector of bottom center.
+            step (int): The number of times the object's speed is calculated.
+            frame_num (int): Total number of frames between 2 calculations.
+            prev_speed (float): Previous speed estimate.
+            scale (float): Scaling factor compared to 720p (1280 x 720px).
 
-    # Speed can be calculated using moving average which is more robust to noisy jitters in instant velocity
-    # due to non-ideal detection and tracking
-    speed_estimate = (prev_speed * step + vi * s) / (step + 1)
+        Returns:
+            float: Estimated speed in miles per hour.
+        """
+        bottom_center = np.array([(det[0] + det[2]) / 2.0, (det[3] - self.y_min)]) / scale
+        box_vel = box_vel / scale
 
-    return speed_estimate
+        # Transform the Velocities using the computed camera to real world equations
+        v_x_trans = (((self.H[0][0] * box_vel[0] + self.H[0][1] * box_vel[1]) *
+                      (self.H[2][0] * bottom_center[0] + self.H[2][1] * bottom_center[1] + self.H[2][2]) -
+                      (self.H[0][0] * bottom_center[0] + self.H[0][1] * bottom_center[1] + self.H[0][2]) *
+                      (self.H[2][0] * box_vel[0] + self.H[2][1] * box_vel[1])) /
+                     ((self.H[2][0] * bottom_center[0] + self.H[2][1] * bottom_center[1] + self.H[2][2]) ** 2))
+
+        v_y_trans = (((self.H[1][0] * box_vel[0] + self.H[1][1] * box_vel[1]) *
+                      (self.H[2][0] * bottom_center[0] + self.H[2][1] * bottom_center[1] + self.H[2][2]) -
+                      (self.H[1][0] * bottom_center[0] + self.H[1][1] * bottom_center[1] + self.H[1][2]) *
+                      (self.H[2][0] * box_vel[0] + self.H[2][1] * box_vel[1])) /
+                     ((self.H[2][0] * bottom_center[0] + self.H[2][1] * bottom_center[1] + self.H[2][2]) ** 2))
+
+        # Scale Recovery
+        s1, s2 = 1.1, 0.9
+        a, b = (s2 - s1) / (self.y_max - self.y_min) * scale, s1
+        s = a * bottom_center[1] + b
+
+        # Velocity calculation
+        instant_speed = np.sqrt(sum([(v_x_trans * self.s_x) ** 2, (v_y_trans * self.s_y) ** 2]))
+
+        # Translating the speed to miles per hour
+        vi = instant_speed / frame_num * self.fps * 9 / 4
+
+        # Suppress noise
+        if vi <= 3.0:
+            vi = 0.0
+
+        # Speed can be calculated using moving average which is more robust to noisy jitters in instant velocity
+        # due to non-ideal detection and tracking
+        speed_estimate = (prev_speed * step + vi * s) / (step + 1)
+
+        return speed_estimate
